ea61a2bc5e3e4213adaa4d76ba3d25792329fc14
William Hingston <will.hingston@gmail.com>
2022-01-15 23:09:06 +0000

    Update build-push.sh

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -16,7 +16,7 @@ bash scripts/docker-login.sh
 docker buildx use multiarch ||  docker buildx create --name multiarch --use
 
 docker buildx build \
-  --platform "linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v8" \
+  --platform "linux/amd64,linux/arm64,linux/arm/v7" \
   --file "./docker-images/${DOCKERFILE}.dockerfile" \
   -t "$use_tag"  \
   -t "$use_dated_tag"  \
